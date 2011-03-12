6c6ced591b0319a4717a44d2a8bab1d68e1124fe
Patrick Wilson-Welsh <patrickwilsonwelsh@gmail.com>
2011-03-12 18:57:40 -0500

    I hate git

--- Refactor_BankOCR_Whack_A_Method/test/whackAmethod/CleanClassTest.java
+++ Refactor_BankOCR_Whack_A_Method/test/whackAmethod/CleanClassTest.java
@@ -25,13 +25,7 @@ public class CleanClassTest extends CleanTestBase {
     this.methodMetrics = classMetric.methods; 
   }
   
-  @Test
-  public void numberOfMethodsOnClass_Is_GreaterThanMin() throws Exception {
-    assertTrue("Expected number of methods on class " + classMetric.className + " to be above minimum of " 
-        + MIN_METHODS_PER_CLASS + " but was " + methodMetrics.size(), methodMetrics.size() >= MIN_METHODS_PER_CLASS);
-  }
-  
-  @Test
+    @Test
   public void numberOfMethodsOnClass_Is_LessThanMaximum() throws Exception {
     assertTrue("Expected number of methods on class " + classMetric.className + " to be below maximum of " 
         + MAX_METHODS_PER_CLASS + " but was " + methodMetrics.size(), methodMetrics.size() < MAX_METHODS_PER_CLASS);
--- Refactor_BankOCR_Whack_A_Method/test/whackAmethod/CleanTestBase.java
+++ Refactor_BankOCR_Whack_A_Method/test/whackAmethod/CleanTestBase.java
@@ -8,7 +8,6 @@ import cyvis.metric.MethodMetric;
 
 public class CleanTestBase {
   public static final int MAX_METHODS_PER_CLASS = 9;
-  public static final int MIN_METHODS_PER_CLASS = 3;
   public static final int MAX_METHOD_COMPLEXITY = 4;
   public static final int MAX_BYTECODE_STATEMENTS = 42; //Thank you Douglas Adams!
   
